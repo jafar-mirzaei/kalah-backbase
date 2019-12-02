1bcde3e59d508f62a4aba7400b7d246a75c70d6e
Jafar Mirzaei <jm.csh2009@gmail.com>
2019-12-03 00:23:25 +0330

    -Create Game Integration test added

---
+++ src/test/java/com/backbase/kalah/kalahassignment/service/rule/ChangeTurnGameRuleExecutorTest.java
@@ -0,0 +1,17 @@
+package com.backbase.kalah.kalahassignment.service.rule;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ChangeTurnGameRuleExecutorTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void executeRule() {
+  }
+}
